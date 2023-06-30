37c2837e5078e434cf6abbd03e7b13649e8a4518
HowardWhile <makubex49gmail.com>
2023-06-30 10:23:52 +0800

    init

---
+++ test.sh
@@ -0,0 +1,39 @@
+
+#!/bin/bash
+
+# 宣告各個參數
+power_management_params=(
+  "cpuidle.off=1"
+  "intel_idle.max_cstate=0"
+  "cpufreq.off=1"
+  "processor.max_cstate=0"
+  "processor_idle.max_cstate=0"
+  "intel_pstate=disable"
+  "rcupdate.rcu_cpu_stall_suppress=1"
+  "idle=poll"
+)
+
+core_isolation_params=(
+  "nohz_full=3"
+  "isolcpus=3"
+  "rcu_nocbs=3"
+  "irqaffinity=0"
+)
+
+graphics_settings_params=(
+  "i915.enable_rc6=0"
+  "i915.enable_dc=0"
+  "i915.disable_power_well=0"
+  "i915.enable_guc=2"
+)
+
+# 生成參數字串
+power_management_str=$(IFS=" " ; echo "${power_management_params[*]}")
+core_isolation_str=$(IFS=" " ; echo "${core_isolation_params[*]}")
+graphics_settings_str=$(IFS=" " ; echo "${graphics_settings_params[*]}")
+
+# 修改 GRUB 設定檔
+sudo sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/\"$/ $power_management_str $core_isolation_str $graphics_settings_str\"/" grub
+
+# 更新 GRUB 設定
+sudo update-grub
